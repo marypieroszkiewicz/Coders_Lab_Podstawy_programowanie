c58a764c9f27b052212e2ab6ddb1f9c6c406ae77
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2017-07-04 14:51:29 +0200

    add a new tasks

---
+++ 1_Zadania/6_Funkcje/js/zadanie5.js
@@ -0,0 +1,30 @@
+var example = function() {
+
+	// Pętla for
+
+	for (var i = 0; i < 10; i++) {
+		console.log('Gdy piszę kod zawszę i wszędzie używam funkcji oraz nie tworzę zmiennych globalnych');
+	}
+
+	// Pętla while
+
+	var x = 1;
+
+	while (x <= 10) {
+		console.log('Gdy piszę kod zawszę i wszędzie używam funkcji oraz nie tworzę zmiennych globalnych');
+    x++;
+
+	}
+
+	// Pętla do while
+
+	var x = 1;
+
+	do {
+		console.log('Gdy piszę kod zawszę i wszędzie używam funkcji oraz nie tworzę zmiennych globalnych');
+    	x++;
+	} while (x <= 10);
+
+};
+
+example();
