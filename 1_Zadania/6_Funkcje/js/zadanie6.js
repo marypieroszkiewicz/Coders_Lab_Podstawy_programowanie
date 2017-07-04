59fb6ff9e193738089ab8cf99e64068c9509aec0
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2017-07-04 16:49:55 +0200

    add a new tasks

--- 1_Zadania/6_Funkcje/js/zadanie6.js
+++ 1_Zadania/6_Funkcje/js/zadanie6.js
@@ -3,57 +3,58 @@ function rentCost(days) {
 	var costOfRoom;
 	var numbersOfWeeks = Math.floor(days / 7);
 
-	for (var i = 0; i < days; i++) {
-		if (days == 1) {
-			costOfRoom = 200;
-		} else if (days <= 3) {
-			costOfRoom = days * 180;
-		} else if (days <= 7) {
-			costOfRoom = days * 160;
-		} else if (days >= 8) {
-			costOfRoom = (days * 150) - (50 * numbersOfWeeks);
-		}
+	if (days == 1) {
+		costOfRoom = 200;
+	} else if (days <= 3) {
+		costOfRoom = days * 180;
+	} else if (days <= 7) {
+		costOfRoom = (days * 160) - (50 * numbersOfWeeks);
+	} else if (days >= 8) {
+		costOfRoom = (days * 150) - (50 * numbersOfWeeks);
 	}
+
 	return costOfRoom;
-	console.log(costOfRoom);
 }
 
+console.log(rentCost(7)); //1070
 console.log(rentCost(8)); //1150
 console.log(rentCost(14)); //2000
 console.log(rentCost(16)); //2300
+
 
 // Funkcja rentCost z wykorzystaniem pętli switch
 
-function rentCost(days) {
+// function rentCost(days) {
 
-	var discount = 50;
-	var numbersOfWeeks = Math.floor(days / 7);
-	var cost = 0;
+// 	var discount = 50;
+// 	var numbersOfWeeks = Math.floor(days / 7);
+// 	var cost = 0;
 
-	switch (days) {
-		case 1:
-			cost = 200;
-			break;
-		case 2:
-		case 3:
-			cost = 180 * days; 
-			break;
-		case 4:
-		case 5:
-		case 6:
-			cost = 160 * days;
-			break;
-		case 7:
-			cost = (160 * days) - discount;
-			break;
-		default:
-			cost = (150 * days) - (discount * numbersOfWeeks);
-			break;
-	}
+// 	switch (days) {
+// 		case 1:
+// 			cost = 200;
+// 			break;
+// 		case 2:
+// 		case 3:
+// 			cost = 180 * days; 
+// 			break;
+// 		case 4:
+// 		case 5:
+// 		case 6:
+// 			cost = 160 * days;
+// 			break;
+// 		case 7:
+// 			cost = (160 * days) - discount;
+// 			break;
+// 		default:
+// 			cost = (150 * days) - (discount * numbersOfWeeks);
+// 			break;
+// 	}
 
-	return cost;
-}
+// 	return cost;
+// }
 
-console.log(rentCost(8)); //1150
-console.log(rentCost(14)); //2000
-console.log(rentCost(16)); //2300
+// console.log(rentCost(7)); //1070
+// console.log(rentCost(8)); //1150
+// console.log(rentCost(14)); //2000
+// console.log(rentCost(16)); //2300
---
+++ 1_Zadania/6_Funkcje/js/zadanie7.js
@@ -0,0 +1,33 @@
+function rockPaperScissors(player1, player2) {
+
+	var paper = 'papier';
+	var scissors = 'nozyce';
+	var rock = 'kamien';
+
+	if (player1 == player2) {
+
+		console.log('Remis');
+
+	} else if (((player1 == paper) && (player2 == rock)) ||
+		((player1 == scissors) && (player2 == paper)) ||
+		((player1 == rock) && (player2 == scissors))) {
+
+			console.log('Player 1 Win');
+
+		} else if (((player1 == rock) && (player2 == paper)) ||
+				   ((player1 == paper) && (player2 == scissors)) ||
+				   ((player1 == scissors) && (player2 == rock))) {
+
+				console.log('Player 2 Win');
+
+		} else {
+
+			console.log('Wrong information');
+		}
+}
+
+rockPaperScissors('papier', 'nozyce');
+rockPaperScissors('nozyce', 'kamien');
+rockPaperScissors('papier', 'kamien');
+rockPaperScissors('papier', 'papier');
+rockPaperScissors('knife', 'kamien');
