dce8310916517da2f85e30ef209637054dcbdc49
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2017-07-06 14:04:33 +0200

    add a new tasks

--- 2_Zadania_domowe/js/zadanie1.js
+++ 2_Zadania_domowe/js/zadanie1.js
@@ -4,20 +4,12 @@ var myArray = [2, 3, 1, 6, 100, 49, 5, 7, 8, 9 ];
 
 function getSecondMaxNumber(array) {
 
+	var arr = array.sort(function(a, b) {
+		return a - b;
+	});
 
-
+	var secondLargest = arr[arr.length - 2];
+		return secondLargest;
 }
 
-
-function processData(myArray) {
-  var sorted = unique.sort(function(a, b) {
-    return a - b;
-  });
-  var result = sorted[sorted.length - 2];
-  return result;
-}
-
-
-var myArray =['20','120','111','215','54','78'];
-var secondLargest = myarray.sort(function(a,b){return a - b})[myArray.length-2];
-console.log(secondLargest); //120;
+console.log(getSecondMaxNumber(myArray));
