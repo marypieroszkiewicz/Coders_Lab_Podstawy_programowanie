9efce7262449709ba80a458cfd06bd2fae950436
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2017-07-07 21:22:48 +0200

    add a new tasks

--- 2_Zadania_domowe/js/zadanie3.js
+++ 2_Zadania_domowe/js/zadanie3.js
@@ -1,16 +1,15 @@
 /* ------- Zadania domowe do samodzielnego wykonania - Funkcje ------- */
 
-var n = 10;
-
 function getEvenSum(n) {
 
 	var sum = 0;
 
 	for (var i = 1; i <= n; i++) {
-		if (i%2 === 0) {
+		if (i % 2 === 0) {
 			sum += i;
 		}
 	}
+	
 	return sum;
 }
 
--- 2_Zadania_domowe/js/zadanie4.js
+++ 2_Zadania_domowe/js/zadanie4.js
@@ -1,1 +1,23 @@
 /* ------- Zadania domowe do samodzielnego wykonania - Funkcje ------- */
+
+function getFibonacciNumbers(n) {
+    
+    var sequence = [0, 1];
+    
+    if (n === 0 || n === 1) {
+        return sequence[n];
+    } 
+    else {
+    	for (var i = 2; i < n; i++ ) {
+        	var sum = sequence[i - 1] + sequence[i - 2];
+        	sequence.push(sum);
+    	}
+
+    return sequence;
+    
+    }
+}
+
+console.log(getFibonacciNumbers(0));
+console.log(getFibonacciNumbers(1));
+console.log(getFibonacciNumbers(9));
