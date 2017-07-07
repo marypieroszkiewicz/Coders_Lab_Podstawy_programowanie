014a8a8c4b28a9ed0f1ee5cb53fa307fe2ce24ab
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2017-07-08 00:22:09 +0200

    add a new tasks

--- 2_Zadania_domowe/js/zadanie6.js
+++ 2_Zadania_domowe/js/zadanie6.js
@@ -1,1 +1,13 @@
 /* ------- Zadania domowe do samodzielnego wykonania - Funkcje ------- */
+
+function getPI(n) {
+
+    var pi = 0;
+
+    for (var i = 0; i < n; i++) {
+        pi = pi + 4 * Math.pow(-1, i) / (2 * i + 1);
+    }
+    return pi;
+}
+
+console.log(getPI(9));
--- 2_Zadania_domowe/js/zadanie7.js
+++ 2_Zadania_domowe/js/zadanie7.js
@@ -1,1 +1,24 @@
 /* ------- Zadania domowe do samodzielnego wykonania - Funkcje ------- */
+
+function whatNumber(n) {
+
+	var sum = 0;
+	var info = '';
+
+	for (var i = 1; i < n; i++) {
+		if (n % i === 0) {
+			console.log(i);
+			sum += i;
+		}
+		if (sum == n) {
+			info = ' doskonała';
+		} else if (sum < n) {
+			info = ' niekompletna';
+		} else {
+			return false;
+		}
+	}
+	console.log(n + info);
+}
+console.log(whatNumber(6));
+console.log(whatNumber(10));
