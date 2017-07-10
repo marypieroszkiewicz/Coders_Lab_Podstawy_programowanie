3bf028900a7d8e9f76671487c24b3485cb57301c
Mary Pieroszkiewicz <mary.pieroszkiewicz@gmail.com>
2017-07-10 10:42:04 +0200

    add a new tasks

--- 2_Zadania_domowe/js/zadanie7.js
+++ 2_Zadania_domowe/js/zadanie7.js
@@ -1,24 +1,34 @@
 /* ------- Zadania domowe do samodzielnego wykonania - Funkcje ------- */
 
+function sumFactors(n) {
+
+    var sum = 0;
+
+    for (var i = 1; i < n; i++) {
+        if ( n % i === 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+function isNumberPerfect(n) {
+
+    if (sumFactors(n) == n) {
+        console.log(n + ' to jest liczba doskonala');
+    }
+    else if (sumFactors(n) < n) {
+        console.log(n + ' to jest liczba niekompletna');
+    }
+    else {
+        console.log(n + ' to nie jest zadna z nich');
+    }
+}
+
 function whatNumber(n) {
 
-	var sum = 0;
-	var info = '';
-
-	for (var i = 1; i < n; i++) {
-		if (n % i === 0) {
-			console.log(i);
-			sum += i;
-		}
-		if (sum == n) {
-			info = ' doskonała';
-		} else if (sum < n) {
-			info = ' niekompletna';
-		} else {
-			return false;
-		}
-	}
-	console.log(n + info);
+    for (var j = 1; j <= n; j++) {
+        isNumberPerfect(j);
+    }
 }
-console.log(whatNumber(6));
-console.log(whatNumber(10));
+whatNumber(100);
